e0963bf7fad1574af0c2bcc9d8d3132052b475ed
A7medMg <asilme023@gmail.com>
2026-01-19 20:35:01 +0200

    added extension on context .

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -12,7 +12,7 @@ class DocDoc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    return ScreenUtilInit(
-    designSize: const Size(360, 690),
+    designSize: const Size(375, 812),
     minTextAdapt: true,
     splitScreenMode: true,
     builder: (context, child) {
