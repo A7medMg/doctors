09985ff41d925bde2efda24e257423bf346b8007
A7medMg <asilme023@gmail.com>
2026-01-25 19:00:45 +0200

    added firebase.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,6 @@
 import 'package:doctors/core/routing/app_routes.dart';
 import 'package:doctors/doc_app.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,6 +9,8 @@ import 'core/di/dependency_injection.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  
+
   setupGetIt();
   runApp( DocDoc(
     appRoutes: AppRoutes(),
